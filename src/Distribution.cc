41d26102d0ce5a5cb8c1e207426ef25c9b59f7f2
ralph <da_viech@yahoo.de>
2019-04-30 17:36:22 +0200

    -

--- src/Distribution.cc
+++ src/Distribution.cc
@@ -61,7 +61,7 @@ DistributeKeypoints(std::vector<cv::KeyPoint> &kpts, const int &minX, const int 
         }
         case DISTRIBUTION_GRID :
         {
-            DistributeKeypointsGrid(kpts, minX, maxX, minY, maxY, N, 30);
+            DistributeKeypointsGrid(kpts, minX, maxX, minY, maxY, N);
             break;
         }
         case DISTRIBUTION_KEEP_ALL :
